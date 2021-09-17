28db91c28925742071aa7e6b9173368974f421e6
codingbitz <bnarayanan84@gmail.com>
2021-09-17 08:31:05 +0530

    goLang

---
+++ programming/go-learning/main/src/hworld.go
@@ -0,0 +1,12 @@
+package main
+// single comment
+import "fmt"
+
+func main() {
+/* Block comment , this program
+    will print hello world
+*/
+    fmt.Println("Hello World!")
+}
+
+// go run /src/hworld.go
