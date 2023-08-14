f30122efdf0fb3e869a6fdcd45cf34dc5a8a6450
codingbitz <bnarayanan84@gmail.com>
2023-08-14 19:53:34 -0400

    Delete aws/ecs-flasky directory

--- aws/ecs-flasky/src/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Home page"
-
-if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=5000)
